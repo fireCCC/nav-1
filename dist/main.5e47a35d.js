58a1b582b3d3af1e88c09961df66f060c65a3715
Amber <944458473@qq.com>
2019-11-19 00:31:54 +0800

    init

--- dist/main.5e47a35d.js
+++ dist/main.5e47a35d.js
@@ -124,11 +124,11 @@ var $lastLi = $siteList.find('li.last');
 var x = localStorage.getItem('x');
 var xObject = JSON.parse(x);
 var hashMap = xObject || [{
-  logo: 'images/figma.svg',
+  logo: 'figma.svg',
   logoType: 'img',
   url: 'https://www.figma.com/'
 }, {
-  logo: 'images/iconfont.ico',
+  logo: 'iconfont.ico',
   logoType: 'img',
   url: 'https://www.iconfont.cn/'
 }];
@@ -199,4 +199,4 @@ $(document).on('keypress', function (e) {
   }
 });
 },{}]},{},["epB2"], null)
-//# sourceMappingURL=main.decdec27.js.map
+//# sourceMappingURL=main.5e47a35d.js.map
